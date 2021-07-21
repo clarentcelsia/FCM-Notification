bdf6198dc761a6255ae2c9b53d859a2ca2166eea
Clarenti Celsia <66846357+clarentclaire@users.noreply.github.com>
2021-07-21 07:04:37 +0700

    Delete Common.kt

--- app/src/main/java/com/example/firebasenotification/Common.kt
+++
@@ -1,9 +0,0 @@
-package com.example.firebasenotification
-
-class Common {
-    companion object{
-        const val BASE_URL = "https://fcm.googleapis.com"
-        const val KEY = "AAAAjJDgoN4:APA91bHNl4qdaeMheVUv8F_VOZNKMiWW30Rt8bftL01oYo08xSR7PnumjeijXmt5fNro_Zy6NfEEzUOqgiTqUS7qvzmsa7iXsy-U8GMdcsKyMFyCkWxQYNooQtSehO2VG_HjtdYRbN8M"
-        const val CONTENT_TYPE = "application/json"
-    }
-}
